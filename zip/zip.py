8c738ca93a0c2619245bd3b38f5cefd2fa3e067e
Vitor Dutra <40544826+vitordutra@users.noreply.github.com>
2020-05-13 17:20:00 -0300

    Zip - Selenium Course - Eduardo Mendes

---
+++ zip/zip.py
@@ -0,0 +1,10 @@
+# By Eduardo Mendes - @dunossauro
+
+seq_a = [1, 2, 3, 4]
+seq_b = [4, 3, 2, 1]
+
+zip_seq_a_b = list(zip(seq_a, seq_b))
+
+print(zip_seq_a_b)
+
+# Output = [(1, 4), (2, 3), (3, 2), (4, 1)]
